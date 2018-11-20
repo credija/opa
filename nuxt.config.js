9b6e6ca8228b74e2005d6a15280fc9691b81ce05
Kevin Aguiar <kevin.aguiar@credija.com.br>
2018-11-20 16:16:57 -0200

    UPDATE: Updated NuxtJS version

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ module.exports = {
 
   build: {
     babel: {
-      presets: ['@nuxtjs/babel-preset-app'],
+      presets: ['@nuxt/babel-preset-app'],
       plugins: [
         [
           'component',
